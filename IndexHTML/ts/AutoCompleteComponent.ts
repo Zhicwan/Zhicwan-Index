c2a5d0eb465dec7b729e87f142aada07c626e845
Zhicheng Wang <zhicwan@microsoft.com>
2018-09-12 01:11:27 +0800

    fix typeahead

--- IndexHTML/ts/AutoCompleteComponent.ts
+++ IndexHTML/ts/AutoCompleteComponent.ts
@@ -15,32 +15,31 @@ export class AutoCompleteComponent {
         this.DisplayLimit = DisplayLimit;
     }
 
-    public Init: () => void = () => {
-        let DataSet: Twitter.Typeahead.Dataset<string> = {
-            source: this.DataSourceProcess,
-            async: this.AutoCompleteConfiguration.IsAsync,
-            limit: this.DisplayLimit
-        };
-        this.AutoCompleteComponentElement.typeahead(TypeaheadDefaultOption, DataSet);
-    }
+    public Init: () => void =
+        () => {
+            let DataSet: Twitter.Typeahead.Dataset<string> = {
+                source: this.DataSourceProcess,
+                async: this.AutoCompleteConfiguration.IsAsync,
+                limit: this.DisplayLimit
+            };
+            this.AutoCompleteComponentElement.typeahead(TypeaheadDefaultOption, DataSet);
+        }
 
-    private DataSourceProcess(
-        queryString: string,
-        SyncProcess: (result: string[]) => void,
-        AsyncProcess?: (result: string[]) => void
-    ): void {
-        let ProcessFunc = SyncProcess;
-        if (this.AutoCompleteConfiguration.IsAsync && AsyncProcess != undefined)
-            ProcessFunc = AsyncProcess;
-        $.ajax({
-            type: this.AutoCompleteConfiguration.RequestType,
-            async: this.AutoCompleteConfiguration.IsAsync,
-            url: this.AutoCompleteConfiguration.RequestUrl,
-            data: this.AutoCompleteConfiguration.RequestDataBuilder(queryString),
-            dataType: this.AutoCompleteConfiguration.ResponseDataType,
-            jsonp: this.AutoCompleteConfiguration.ResponseJsonpName,
-            success: this.AutoCompleteConfiguration.ResponseDataExtractor(ProcessFunc)
-        });
-    }
+    private DataSourceProcess:
+        (queryString: string, SyncProcess: (result: string[]) => void, AsyncProcess?: (result: string[]) => void) => void =
+        (queryString: string, SyncProcess: (result: string[]) => void, AsyncProcess?: (result: string[]) => void) => {
+            let ProcessFunc = SyncProcess;
+            if (this.AutoCompleteConfiguration.IsAsync && AsyncProcess != undefined)
+                ProcessFunc = AsyncProcess;
+            $.ajax({
+                type: this.AutoCompleteConfiguration.RequestType,
+                async: this.AutoCompleteConfiguration.IsAsync,
+                url: this.AutoCompleteConfiguration.RequestUrl,
+                data: this.AutoCompleteConfiguration.RequestDataBuilder(queryString),
+                dataType: this.AutoCompleteConfiguration.ResponseDataType,
+                jsonp: this.AutoCompleteConfiguration.ResponseJsonpName,
+                success: this.AutoCompleteConfiguration.ResponseDataExtractor(ProcessFunc)
+            });
+        }
 }
 
--- IndexHTML/ts/Config.ts
+++ IndexHTML/ts/Config.ts
@@ -8,8 +8,8 @@
 const RequireJsConfiguration: RequireConfig = {
     baseUrl: "js/",
     paths: {
-        jquery: "https://cdn.bootcss.com/jquery/3.3.1/jquery.min",
-        typeahead: "https://cdn.bootcss.com/typeahead.js/0.11.1/typeahead.jquery.min"
+        "jquery": "https://cdn.bootcss.com/jquery/3.3.1/jquery.min",
+        "typeahead": "libs/typeahead.jquery.min"
     }
 };
 
